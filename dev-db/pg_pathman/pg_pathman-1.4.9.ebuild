e32feec57d5681d8ff6de4e80f451c78c5d5d74d
Dan Molik <dan@d3fy.net>
2018-01-24 16:05:52 -0500

    Add pg_pathman

---
+++ dev-db/pg_pathman/pg_pathman-1.4.9.ebuild
@@ -0,0 +1,40 @@
+# Copyright 1999-2018 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=6
+
+POSTGRES_COMPAT=( 10 )
+POSTGRES_USEDEP="server"
+
+inherit eutils postgres-multi versionator
+
+SLOT="0"
+
+DESCRIPTION="Partitioning tool for PostgreSQL"
+HOMEPAGE="https://github.com/postgrespro/pg_pathman"
+SRC_URI="https://github.com/postgrespro/${PN}/archive/${PV}.tar.gz"
+
+LICENSE="BSD"
+KEYWORDS="~amd64 ~x86"
+IUSE="static-libs"
+REQUIRED_USE="${POSTGRES_REQ_USE}"
+
+DEPEND="
+	${POSTGRES_DEP}
+"
+RDEPEND="${DEPEND}"
+
+src_prepare() {
+	eapply_user
+	postgres-multi_src_prepare
+}
+
+src_compile() {
+	postgres-multi_foreach emake USE_PGXS=1
+}
+
+src_install() {
+	postgres-multi_foreach emake DESTDIR="${D}" install USE_PGXS=1
+
+	use static-libs || find "${ED}" -name '*.a' -delete
+}
--- update_readme.sh
+++ update_readme.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-for i in $(find  -name \*ebuild|sort|sed -e 's/\-[0-9\.r\-]\+\.ebuild//'|uniq|sed -e 's/^\.\/\S\+-\S\+\///'|sort) ; do ebuild=$(find -name \*$i\*ebuild|tail -n1); if [ $ebuild =~ "dev-perl" ] ; then continue ; fi ; site=$(grep HOMEPAGE $ebuild|sed -e 's/^HOMEPAGE=//' -e 's/"//g') ; desc=$(grep DESCRIPTION $ebuild|sed -e 's/^DESCRIPTION=//' -e 's/"//g'); echo -e "  - [${i}](${site})\n    ${desc}" >> README.md; done
+for i in $(find  -name \*ebuild|sort|sed -e 's/\-[0-9\.r\-]\+\.ebuild//'|uniq|sed -e 's/^\.\/\S\+-\S\+\///'|sort) ; do
+	ebuild=$(find -name \*$i\*ebuild|tail -n1)
+	if [ $ebuild =~ "dev-perl" ] ; then
+		continue
+	fi
+	site=$(grep HOMEPAGE $ebuild|sed -e 's/^HOMEPAGE=//' -e 's/"//g')
+	desc=$(grep DESCRIPTION $ebuild|sed -e 's/^DESCRIPTION=//' -e 's/"//g')
+	echo -e "  - [${i}](${site})\n    ${desc}" >> README.md
+done
